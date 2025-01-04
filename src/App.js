39e0b23b1a053a811d15849abfccad0066a5f1b6
Balakrishna Mangala <balakrishnamangala05@gmail.com>
2025-01-04 18:51:44 -0500

    updated Home section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Home.css";
+import { Typewriter } from "react-simple-typewriter";
+import profileImage from "../assets/profile.png"; // Transparent image
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-text">
+        <h1 className="animated-heading">Balakrishna Mangala</h1>
+        <h2>
+          I'm a,{" "}
+          <span className="dynamic-text">
+            <Typewriter
+              words={[
+                "Software Engineer",
+                "Web Developer",
+                "Full-Stack Developer",
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </h2>
+        <div className="home-description">
+          <p>⭐ A Computer Science Student passionate about building and enhancing software products.</p>
+          <p>⭐ I love coding, learning new technologies, and solving challenging problems!</p>
+          <p>⭐ I live in Baltimore, Maryland.</p>
+        </div>
+        <a
+          href="/assets/resume.pdf"
+          download="Balakrishna_Mangala_Resume.pdf"
+          className="resume-btn animated-resume-btn"
+        >
+          Resume
+        </a>
+      </div>
+
+      <div className="home-image">
+        <img
+          src={profileImage}
+          alt="Balakrishna Mangala"
+          className="profile-image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
